8cef8a5c1d48b3d51977b42a40da7ad9d3e9175e
Londoño <98825779+CamiloLonM@users.noreply.github.com>
2022-09-23 17:10:01 -0500

    Category (#5)
    * adding google signIn
    
    * added create category
    
    * finish category
    
    * finish-category
    
    * revert commits fix
    
    * finish category

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,6 +1,30 @@
 
 const { Category } = require('../models')
 
+const allCategories = async (req, res) => {
+
+    const { limit = 5, since = 0 } = req.query;
+    const query = { status: true };
+    const [total, categories] = await Promise.all([
+        Category.countDocuments(query),
+        Category.find(query)
+            .populate('user', 'name')           // Para mostrar el creador de la categoria
+            .skip(Number(since))
+            .limit(Number(limit))
+    ])
+    res.json({
+        total,
+        categories
+    })
+}
+
+const idCategory = async (req, res) => {
+    const { id } = req.params
+    const category = await Category.findById(id)
+        .populate('user', 'name')
+    return res.json(category)
+}
+
 const createCategory = async (req, res) => {
     const name = req.body.name.toUpperCase()
     // Si la categoria existe
@@ -11,17 +35,35 @@ const createCategory = async (req, res) => {
     //Data a guardar
     const data = {
         name,
-        user: req.user._id                  // como mongo esta grabando el ID
+        user: req.user._id                  // // Como estoy grabando el id en mongo
     }
     // crear categoria
-    const category = await new Category(data)
+    const category = new Category(data)
     //Guardar en DB
     await category.save()
-
     res.status(201).json(category)
-
 }
 
+const updateCategory = async (req, res) => {
+    const { id } = req.params
+    const { status, user, ...data } = req.body
+    data.name = data.name.toUpperCase()
+    data.user = req.user._id
+    const category = await Category.findByIdAndUpdate(id, data, { new: true })
+    res.status(201).json(category)
+}
+
+const deleteCategory = async (req, res) => {
+    const { id } = req.params
+    const category = await Category.findByIdAndUpdate(id, { status: false }, { new: true })
+    res.json(category)
+}
+
+
 module.exports = {
-    createCategory
+    allCategories,
+    createCategory,
+    idCategory,
+    updateCategory,
+    deleteCategory
 }
